ba394db238b429747275ef5262ebcf65f2999ffc
Gacki1 <kernification@gmail.com>
2025-12-12 15:28:23 +0100

    Added login-backend with some files for basic login

---
+++ main/login-backend/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.post("/api/login", (req, res) => {
+    const { username, password} = req.body;
+
+    if (username === "test" && password === "123") {
+        console.log("Login Erfolgreich");
+        res.status(200),json({ success: true, redirect: "/main.html"});
+    } else {
+        console.log("Login Fehlgeschlagen");
+        res.status(401).json({ success: false, message: "Falscher Benutzername oder Passwort"});
+    }
+});
+
+app.listen(port, () => {
+    console.log("Login Backend läuft auf http://localhost:${port}");
+});
