dc3f5ba6fb6e96f8a96add387b62f59c4e05e246
Gacki1 <kernification@gmail.com>
2025-12-12 18:22:51 +0100

    test

--- main/backend/backend/urls.py
+++ main/backend/backend/urls.py
@@ -21,5 +21,6 @@ from .views import login_view
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path("api/login", login_view),
+    path("/api/login", login_view),
+    path("/api/login/", login_view),
 ]
