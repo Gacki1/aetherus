85eef8ca5672623c753a35ba06503b7303552a98
Gacki1 <kernification@gmail.com>
2025-12-13 17:35:01 +0100

    Fixed settings.py

--- main/backend/backend/settings.py
+++ main/backend/backend/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "djoser",
-    "rest_framework_authtoken"
+    "rest_framework.authtoken",
 ]
 
 from datetime import timedelta
