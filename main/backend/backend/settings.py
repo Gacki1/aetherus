ff352e0639632632115b4cb038aae72bc3960964
Gacki1 <kernification@gmail.com>
2025-12-13 18:58:38 +0100

    Fixed settings.py

--- main/backend/backend/settings.py
+++ main/backend/backend/settings.py
@@ -66,7 +66,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
     ),
     "DEFAULT_PERMISSION_CLASSES": (
-        "rest_framework.persmissions.IsAuthenticated",
+        "rest_framework.permissions.IsAuthenticated",
     ),
 }
 
