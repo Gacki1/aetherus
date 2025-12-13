7ccef4e090f404bf75495fb2ac276358cf8182e5
Gacki1 <kernification@gmail.com>
2025-12-13 22:07:47 +0100

    Fixed

--- main/backend/backend/views.py
+++ main/backend/backend/views.py
@@ -15,6 +15,7 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_str
 from django.contrib.auth.tokens import default_token_generator
 from django.conf import settings
+from django.core.mail import send_mail
 
 # --- Deine bestehenden Page-Views ---
 
