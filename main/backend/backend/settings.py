682918137e81ca1f629b28675c5c32b1043ab1fc
Gacki1 <kernification@gmail.com>
2025-12-13 22:12:57 +0100

    Fixed

--- main/backend/backend/settings.py
+++ main/backend/backend/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
