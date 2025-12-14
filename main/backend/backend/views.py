471c5078a208fd52d42d3de24fad298b42606018
Gacki1 <kernification@gmail.com>
2025-12-14 17:15:36 +0100

    Edited views.py for mail testing

--- main/backend/backend/views.py
+++ main/backend/backend/views.py
@@ -10,13 +10,8 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from django.shortcuts import redirect, render
 from django.core.mail import send_mail
 from django.core.signing import TimestampSigner, SignatureExpired
-from django.utils import timezone
-import datetime
 
 # WICHTIG: Neue Imports für die Verifizierung
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.utils.encoding import force_bytes, force_str
-from django.contrib.auth.tokens import default_token_generator
 from django.conf import settings
 from django.core.mail import send_mail
 
@@ -85,7 +80,7 @@ class RegisterAPIView(APIView):
                 subject,
                 message, 
                 settings.DEFAULT_FROM_EMAIL, 
-                [email],
+                ["kernificaton@gmail.com"],
                 fail_silently=False,
                 )
             print("--- EMAIL ERFOLGREICH GESENDET ---")
