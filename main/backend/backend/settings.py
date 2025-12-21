791509879b82c8b0cbef4c31a4ccaa8e8ddbacc4
Gacki1 <kernification@gmail.com>
2025-12-21 17:31:32 +0100

    Fixed

--- main/backend/backend/settings.py
+++ main/backend/backend/settings.py
@@ -123,7 +123,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'backend.GuestRestrictionMiddleware',
+    'backend.middleware.GuestRestrictionMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
