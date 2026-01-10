393568acfac958a6b1f982cc25b0ad39b73887b3
Gacki1 <kernification@gmail.com>
2026-01-10 22:26:47 +0100

    Optimized some files

--- main/backend/backend/settings.py
+++ main/backend/backend/settings.py
@@ -101,12 +101,9 @@ DJOSER = {
 }
 
 CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://redis:6379/1", # 'redis' ist der Name aus docker-compose
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://redis:6379',
     }
 }
 
@@ -264,3 +261,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
